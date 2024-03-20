8ac295cd009d34e53a31e3ec2299ec946a06c813
Evan Hearne <ehearne@redhat.com>
2024-03-20 09:41:55 +0000

    moved route53 creation code in main to where the other controller creation code goes in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -118,6 +118,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.Route53Reconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Route53")
+		os.Exit(1)
+	}
+
 	// Disable webhook if environment variable ENABLE_WEBHOOKS is set to false.
 
 	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
@@ -125,13 +133,6 @@ func main() {
 			setupLog.Error(err, "unable to create webhook", "webhook", "CronJob")
 			os.Exit(1)
 		}
-	}
-	if err = (&controller.Route53Reconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Route53")
-		os.Exit(1)
 	}
 	//+kubebuilder:scaffold:builder
 
