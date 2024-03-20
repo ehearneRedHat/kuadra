d18a049f4ef648567a4a10bd72418c655eb1db0d
Evan Hearne <ehearne@redhat.com>
2024-03-20 09:39:13 +0000

    added route53 api skeleton

--- cmd/main.go
+++ cmd/main.go
@@ -126,6 +126,13 @@ func main() {
 			os.Exit(1)
 		}
 	}
+	if err = (&controller.Route53Reconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Route53")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
