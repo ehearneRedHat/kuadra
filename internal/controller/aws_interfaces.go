3a0feaca49f79a7bf20313f1e352638fdcd30ffb
Evan Hearne <ehearne@redhat.com>
2024-03-21 13:28:15 +0000

    added route53 wrapper interface with signature methods

--- internal/controller/aws_interfaces.go
+++ internal/controller/aws_interfaces.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/aws/aws-sdk-go-v2/service/iam/types"
+	route53Types "github.com/aws/aws-sdk-go-v2/service/route53/types"
 	"github.com/aws/smithy-go/middleware"
 )
 
@@ -21,4 +22,17 @@ type IamWrapper interface {
 	DeleteLoginProfileIfExists(ctx context.Context, userName string) error
 	ListAccessKeys(ctx context.Context, userName string) ([]types.AccessKeyMetadata, error)
 	DeleteAccessKeyIfExists(ctx context.Context, userName string, keyId string) error
+}
+
+type Route53Wrapper interface {
+	// Checks to see if root domain exists for given sub domain.
+	IsExistingRootDomain(ctx context.Context, subDomain string) (bool, error)
+	// Gives us the delegation set after creation of hosted zone, which contains the nameserver entries to add to the root domain.
+	CreateHostedZone(ctx context.Context, name string, isPublicHostedZone bool) (route53Types.DelegationSet, error)
+	// Adds the nameservers to the root domain if root domain exists for given subdomain.
+	AddNameserversToRootDomain(ctx context.Context, nameservers []string) error
+	// Deletes the hosted zone by domain name.
+	DeleteHostedZone(ctx context.Context, name string) error
+	// Deletes the associated nameservers of the given domain from the root domain.
+	DeleteNameserversFromRootDomain(ctx context.Context, name string) error
 }
